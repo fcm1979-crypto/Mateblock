7a2cb1a88b34f869668d7f47a5a25f01ec79e03a
fcm1979-crypto <fcm1979@gmail.com>
2026-06-01 19:13:44 +0200

    Estrellas mascota + limpiar scripts py

--- fix_music.py
+++
@@ -1,9 +0,0 @@
-# La música debe estar en index.html directamente, ya está.
-# El problema es que la consola apunta al iframe, no al index.
-# Verificamos que index.html tiene el script
-with open('index.html', 'r') as f:
-    content = f.read()
-if 'MUSICA MATEBLOCKS' in content:
-    print("OK - index.html tiene la música")
-else:
-    print("FALTA - index.html no tiene la música")
